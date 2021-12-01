4a80d358df98170253566109765c9f8b091d1e00
Roy Corella Sanchez <corellasanchez@gmail.com>
2021-12-01 10:58:39 -0600

    Migration

--- database/migrations/2021_11_26_180943_add-reset-code-to-users.php
+++ database/migrations/2021_11_26_180943_add-reset-code-to-users.php
@@ -14,7 +14,7 @@ class AddResetCodeToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid('reset-code');
+            $table->uuid('reset-code')->nullable();
         });
     }
 
